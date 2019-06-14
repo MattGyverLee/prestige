16705e7ffe5af349ba03de53759a7a6e1df4b808
Matthew Lee <matthew_lee@sil.org>
2019-06-14 13:18:58 -0500

    Cherry-Picking changes
    Note: Hot-Loader Un-done

--- src/model/player.tsx
+++ src/model/player.tsx
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { connect } from 'react-redux'
 import ReactPlayer from "react-player"
 import { UpdatePlayerParam } from "../App";
+import '../App.css';
+
+
 //updatePlayerAction,
 
 
@@ -32,6 +35,7 @@ interface PlayProps {
     refreshApp?: (event: UpdatePlayerParam) => void
   }
 
+const valid: boolean = true;
 class PlayerZone extends Component<PlayProps> {
 
     private player!:ReactPlayer
@@ -82,7 +86,7 @@ class PlayerZone extends Component<PlayProps> {
     ref = (player: any) => {
         this.player = player
       }
-
+    
     render() {
     return (
        <div>
@@ -111,66 +115,83 @@ class PlayerZone extends Component<PlayProps> {
                     onDuration={this.onDuration}
                     />
             </div>
-            <table><tbody>
-                <tr>
-                <th>Controls</th>
-                <td>
-                    <button onClick={this.props.stopPlaying}>Stop</button>
-                    <button onClick={this.props.playPause}>{this.props.playing ? 'Pause' : 'Play'}</button>
-                    <button onClick={this.onClickFullscreen}>Fullscreen</button>
-                </td>
-                </tr>
-                <tr>
-                <th>Speed</th>
-                <td>
-                    <button onClick={this.setPlaybackRate} value={1}>1x</button>
-                    <button onClick={this.setPlaybackRate} value={1.5}>1.5x</button>
-                    <button onClick={this.setPlaybackRate} value={2}>2x</button>
-                </td>
-                </tr>
-                <tr>
-                <th>Seek</th>
-                <td>
-                    <input
-                    type='range' min={0} max={1} step='any'
-                    value={this.props.played}
-                    onMouseDown={this.onSeekMouseDown}
-                    onChange={this.onSeekChange}
-                    onMouseUp={this.onSeekMouseUp}
-                    />
-                </td>
-                </tr>
-                <tr>
-                <th>Volume</th>
-                <td>
-                    <input type='range' min={0} max={1} step='any' value={this.props.volume} onChange={this.setVolume} />
-                </td>
-                </tr>
-                <tr>
-                <th>
-                    <label htmlFor='muted'>Muted</label>
-                </th>
-                <td>
-                    <input id='muted' type='checkbox' checked={this.props.muted} onChange={this.toggleMuted} />
-                </td>
-                </tr>
-                <tr>
-                <th>
-                    <label htmlFor='loop'>Loop</label>
-                </th>
-                <td>
-                    <input id='loop' type='checkbox' checked={this.props.loop} onChange={this.props.toggleLoop} />
-                </td>
-                </tr>
-                <tr>
-                <th>Played</th>
-                <td><progress max={1} value={this.props.played} /></td>
-                </tr>
-                <tr>
-                <th>Loaded</th>
-                <td><progress max={1} value={this.props.loaded} /></td>
-                </tr>
-            </tbody></table>
+            <div className="audio-Layers">
+            <div>
+                <input type="radio" id="contactChoice1"
+                    name="contact" value="email"/>
+                <label htmlFor="contactChoice1">Translation</label>
+                <input type="radio" id="contactChoice2"
+                    name="contact" value="phone"/>
+                <label htmlFor="contactChoice2">Original</label>
+                <input type="radio" id="contactChoice3"
+                    name="contact" value="mail"/>
+                <label htmlFor="contactChoice3">Careful</label>
+            </div>
+            </div>
+            <div className="mediaControls">
+            <table>
+                <tbody>
+                    <tr>
+                    <th>Controls</th>
+                    <td>
+                        <button onClick={this.props.stopPlaying}>Stop</button>
+                        <button onClick={this.props.playPause}>{this.props.playing ? 'Pause' : 'Play'}</button>
+                        <button onClick={this.onClickFullscreen}>Fullscreen</button>
+                    </td>
+                    </tr>
+                    <tr>
+                    <th>Speed</th>
+                    <td>
+                        <button onClick={this.setPlaybackRate} value={1}>1x</button>
+                        <button onClick={this.setPlaybackRate} value={1.5}>1.5x</button>
+                        <button onClick={this.setPlaybackRate} value={2}>2x</button>
+                    </td>
+                    </tr>
+                    <tr>
+                    <th>Seek</th>
+                    <td>
+                        <input
+                        type='range' min={0} max={1} step='any'
+                        value={this.props.played}
+                        onMouseDown={this.onSeekMouseDown}
+                        onChange={this.onSeekChange}
+                        onMouseUp={this.onSeekMouseUp}
+                        />
+                    </td>
+                    </tr>
+                    <tr>
+                    <th>Volume</th>
+                    <td>
+                        <input type='range' min={0} max={1} step='any' value={this.props.volume} onChange={this.setVolume} />
+                    </td>
+                    </tr>
+                    <tr>
+                    <th>
+                        <label htmlFor='muted'>Muted</label>
+                    </th>
+                    <td>
+                        <input id='muted' type='checkbox' checked={this.props.muted} onChange={this.toggleMuted} />
+                    </td>
+                    </tr>
+                    <tr>
+                    <th>
+                        <label htmlFor='loop'>Loop</label>
+                    </th>
+                    <td>
+                        <input id='loop' type='checkbox' checked={this.props.loop} onChange={this.props.toggleLoop} />
+                    </td>
+                    </tr>
+                    <tr>
+                    <th>Played</th>
+                    <td><progress max={1} value={this.props.played} /></td>
+                    </tr>
+                    <tr>
+                    <th>Loaded</th>
+                    <td><progress max={1} value={this.props.loaded} /></td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
         </div>
         );
     }
