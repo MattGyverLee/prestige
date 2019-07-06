28459a78e290da9a01049d16398066764acba98f
Matthew Lee <Matthew_Lee@sil.org>
2019-07-06 01:36:34 -0500

    First Attempt @ Cleaning Deps

--- craco.config.electron.js
+++ craco.config.electron.js
@@ -1,7 +1,12 @@
-module.exports = {
-  webpack: {
-    configure: {
-      target: "electron-renderer"
-    }
-  }
+module.exports = config => {
+  config.target = "electron-renderer";
+  config.plugins = [
+    new webpack.ProvidePlugin({
+      WaveSurfer: "wavesurfer.js"
+    }),
+    new webpack.DefinePlugin({
+      "process.env.FLUENTFFMPEG_COV": false
+    })
+  ];
+  return config;
 };
--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,16 @@
+let webpack = require("webpack");
 module.exports = {
   webpack: {
     configure: {
       target: "electron-renderer"
-    }
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        WaveSurfer: "wavesurfer.js"
+      }),
+      new webpack.DefinePlugin({
+        "process.env.FLUENTFFMPEG_COV": false
+      })
+    ]
   }
 };
