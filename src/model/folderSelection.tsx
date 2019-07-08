05b59e54a637c57e4a29f0559043292d84d08026
Blaine Billings <blaine.t.billings@gmail.com>
2019-07-08 15:21:25 -0500

    Working Concatenation

--- src/model/folderSelection.tsx
+++ src/model/folderSelection.tsx
@@ -6,6 +6,7 @@ import { getTimelineIndex, sourceMedia } from "./globalFunctions";
 
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
+import { isForXStatement } from "@babel/types";
 
 var watcherRef: any;
 interface StateProps {
@@ -371,12 +372,8 @@ class SelectFolderZone extends Component<FolderProps> {
     let ffprobePath = "";
     // Determines Location for Ffmpeg and Ffprobe from environment variables.
     if (isDev) {
-      ffmpegPath =
-        process.cwd() +
-        "\\node_modules\\ffmpeg-static-electron\\bin\\win\\x64\\ffmpeg.exe";
-      ffprobePath =
-        process.cwd() +
-        "\\node_modules\\ffprobe-static-electron\\bin\\win\\x64\\ffprobe.exe";
+      ffmpegPath = process.cwd() + "\\bin\\win\\x64\\ffmpeg.exe";
+      ffprobePath = process.cwd() + "\\bin\\win\\x64\\ffprobe.exe";
     } else {
       // https://stackoverflow.com/questions/33152533/bundling-precompiled-binary-into-electron-app Tsuringa's answer
       // Do I want a relative (__dirname) or absolute (process.cwd()) path?
@@ -435,19 +432,27 @@ class SelectFolderZone extends Component<FolderProps> {
     }
     // Builds MergedAudio object with list of inputs.
     let mergedAudio = fluentFfmpeg();
-    inputFiles.forEach(
-      (v: string) =>
-        (mergedAudio = mergedAudio
-          .addInput(v)
-          .complexFilter("loudnorm=I=-16:TP=-1.5:LRA=11"))
-    );
+    let cf = "";
+    let idx = 0;
+    inputFiles.forEach((v: string) => {
+      mergedAudio = mergedAudio.addInput(v);
+      let letter = idx ? "b" : "out";
+      cf +=
+        "[" +
+        idx.toString() +
+        "]loudnorm=I=-16:TP=-1.5:LRA=11[" +
+        letter +
+        "];";
+      if (idx) {
+        cf += "[out][b]concat=v=0:a=1[out];";
+      }
+      idx += 1;
+    });
+    cf = cf.substring(0, cf.lastIndexOf(";"));
     if (inputFiles.length > 0) {
       mergedAudio._inputs.forEach((v: any, idx: number) => {
         mergedAudio.ffprobe(idx, function(err: any, metadata: any) {
-          const name = v.source.substring(
-            v.source.lastIndexOf(path.sep) + 1,
-            v.source.length
-          );
+          const name = v.source.substring(v.source.lastIndexOf(path.sep) + 1);
           // Keeps a table of contents
           inputTimes.push({
             file: v.source,
@@ -459,36 +464,33 @@ class SelectFolderZone extends Component<FolderProps> {
             stop: lastTime + roundIt(currentDuration(metadata), 3)
           });
           lastTime = lastTime + roundIt(currentDuration(metadata), 3);
+          if (idx === mergedAudio._inputs.length - 1) {
+            // Dumps TOC for later use (temp solution, sometimes writes blank string when not debugging).
+            fs.writeFile(
+              dir +
+                (carefulOrTranslation ? "Careful" : "Translation") +
+                "_Merged.json",
+              JSON.stringify(inputTimes, null, 2),
+              function(err: any) {
+                if (err) {
+                  console.error("Oops");
+                }
+              }
+            );
+          }
           if (err) {
             console.error("Oops");
           }
         });
       });
-      // Dumps TOC for later use (temp solution, sometimes writes blank string when not debugging).
-      fs.writeFile(
-        dir +
-          (carefulOrTranslation ? "Careful" : "Translation") +
-          "_Merged.json",
-        JSON.stringify(inputTimes, null, 2),
-        function(err: any) {
-          if (err) {
-            console.error("Oops");
-          }
-        }
-      );
       // Writes concatenated audio to compressed MP3
       mergedAudio
         .format("mp3")
         .audioBitrate("128k")
         .audioChannels(1)
         .audioCodec("libmp3lame")
-        .mergeToFile(
-          dir +
-            (carefulOrTranslation ? "Careful" : "Translation") +
-            "_Merged.mp3",
-          dir
-        )
-        .outputOptions("-y")
+        .outputOptions(["-map [out]", "-y"])
+        .complexFilter(cf)
         .on("start", function(command: any) {
           console.log("ffmpeg process started:", command);
         })
@@ -497,7 +499,13 @@ class SelectFolderZone extends Component<FolderProps> {
         })
         .on("end", function() {
           console.log("Merging finished!");
-        });
+        })
+        .save(
+          "" +
+            dir +
+            (carefulOrTranslation ? "Careful" : "Translation") +
+            "_Merged.mp3"
+        );
     }
   };
 
